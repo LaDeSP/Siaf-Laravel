35484482de161c15ff10c62e5f4ba2051c29996d
lucineide <anderson.pereira.neves1@gmail.com>
2018-12-03 18:40:45 -0300

    icones

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -14,7 +14,7 @@
 
 		.menu{
 			background-color: #f8f8f8;
-			font-size:1.7vw;
+			font-size:1.1vw;
 		}
 		.rodape{
 			background-color: #80808099;
@@ -25,6 +25,9 @@
 			border-left: 1px solid #6178844f;
 			border-top: 1px solid #6178844f;
 			
+		}
+		img{
+			padding: 10px;
 		}
 	</style>
 </head>
@@ -43,14 +46,14 @@
 		</div>
 		<div  class="row">
 
-			<div  class="col-2 col-sm-2 col-md-3 col-xl-3 menu">
+			<div  class="col-1 col-sm-2 col-md-2 col-xl-2 menu">
 				<div class="row">
 					
-					<div class=" col-md-6 ">
+					<div class=" col-md-5 ">
 						<a href="/home">
 						 <div class="row"> 
 						 	<div>
-						 		<img class="col-md-12 img-fluid" src="/images/agr.png" >	
+						 		<img class="col-md-12 img-fluid" src="/images/Inicio.png" >	
 						 	</div>
 						 	
 						 </div>
@@ -60,11 +63,11 @@
 						</a>
 					</div>
 					
-					<div class=" col-md-6 ">
+					<div class=" col-md-5 ">
 					<a href="/venda">	
 					 <div class="row"> 
 					 	<div>
-					 		<img class="col-md-12 img-fluid" src="/images/agr.png" >	
+					 		<img class="col-md-12 img-fluid" src="/images/Vendas.png" >	
 					 	</div>
 					 	
 					 </div>
@@ -75,11 +78,11 @@
 					</div>
 				</div>
 				<div class="row"> 
-					<div class=" col-md-6 ">
+					<div class=" col-md-5 ">
 					<a href="/estoque">
 					 <div class="row"> 
 					 	<div>
-					 		<img class="col-md-12 img-fluid" src="/images/agr.png" >	
+					 		<img class="col-md-12 img-fluid" src="/images/Estoque.png" >	
 					 	</div>
 					 	
 					 </div>
@@ -89,11 +92,11 @@
 					</a>
 					</div>
 
-					<div class=" col-md-6 ">
+					<div class=" col-md-5 ">
 					<a href="/plantio">
 					 <div class="row"> 
 					 	<div>
-					 		<img class="col-md-12 img-fluid" src="/images/agr.png" >	
+					 		<img class="col-md-12 img-fluid" src="/images/Plantio.png" >	
 					 	</div>
 					 	
 					 </div>
@@ -104,11 +107,11 @@
 					</div>
 				</div>
 				<div class="row"> 
-					<div class=" col-md-6 ">
+					<div class=" col-md-5 ">
 					<a href="/manejo">
 					 <div class="row"> 
 					 	<div>
-					 		<img class="col-md-12 img-fluid" src="/images/agr.png" >	
+					 		<img class="col-md-12 img-fluid" src="/images/Manejo.png" >	
 					 	</div>
 					 	
 					 </div>
@@ -118,11 +121,11 @@
 					</a>	
 					</div>
 
-					<div class=" col-md-6 ">
+					<div class=" col-md-5 ">
 					<a href="/propriedade">
 					 <div class="row"> 
 					 	<div>
-					 		<img class="col-md-12 img-fluid" src="/images/agr.png" >	
+					 		<img class="col-md-12 img-fluid" src="/images/Propriedade.png" >	
 					 	</div>
 					 	
 					 </div>
@@ -133,11 +136,11 @@
 					</div>
 				</div>
 				<div class="row"> 
-					<div class=" col-md-6 ">
+					<div class=" col-md-5 ">
 					<a href="/despesa">
 					 <div class="row"> 
 					 	<div>
-					 		<img class="col-md-12 img-fluid" src="/images/agr.png" >	
+					 		<img class="col-md-12 img-fluid" src="/images/Despesas.png" >	
 					 	</div>
 					 	
 					 </div>
@@ -147,11 +150,11 @@
 					 </a>	
 					</div>
 
-					<div class=" col-md-6 ">
+					<div class=" col-md-5 ">
 					<a href="/investimento">
 					 <div class="row"> 
 					 	<div>
-					 		<img class="col-md-12 img-fluid" src="/images/agr.png" >	
+					 		<img class="col-md-12 img-fluid" src="/images/Investimento.png" >	
 					 	</div>
 					 	
 					 </div>
@@ -163,11 +166,11 @@
 				</div>
 
 				<div class="row"> 
-					<div class=" col-md-6 ">
+					<div class=" col-md-5 ">
 					<a href="/relatorio">
 					 <div class="row"> 
 					 	<div>
-					 		<img class="col-md-12 img-fluid" src="/images/agr.png" >	
+					 		<img class="col-md-12 img-fluid" src="/images/Relatorio.png" >	
 					 	</div>
 					 	
 					 </div>
@@ -177,22 +180,22 @@
 					 </a>	
 					</div>
 
-					<div class=" col-md-6 ">
+					<div class=" col-md-5 ">
 					 <div class="row"> 
 					 	<div>
-					 			
+					 		<img class="col-md-12 img-fluid" src="/images/Manual.png" >	
 					 	</div>
 					 	
 					 </div>
 					 <div class="row">
-					 		
+					 	 <div  class=" col-md-12 text-center ">Manual</div>	
 					 </div>	
 					</div>
 				</div>
 																				
 			</div>
 
-			<div  class="conteudo col-10 col-sm-10 col-md-9 col-xl-9">
+			<div  class="conteudo col-11 col-sm-11 col-md-11 col-xl-10">
 				  @yield('conteudo')
 
 			</div>

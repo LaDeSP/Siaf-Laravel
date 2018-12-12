e38b24a1e8e881419f7242d640e872eefbbf206b
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2018-12-12 09:17:55 -0300

    Configuração.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -38,10 +38,16 @@ class ComposerStaticInit01866e94038b84acd1c89df5cf260e47
         array (
             'phpDocumentor\\Reflection\\' => 25,
         ),
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
         'g' => 
         array (
             'geekcom\\ValidatorDocs\\' => 22,
         ),
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
         'Z' => 
         array (
             'Zend\\Diactoros\\' => 15,
@@ -109,6 +115,7 @@ class ComposerStaticInit01866e94038b84acd1c89df5cf260e47
             'League\\Flysystem\\' => 17,
             'Lcobucci\\JWT\\' => 13,
             'Laravel\\Tinker\\' => 15,
+            'LaravelLegends\\PtBrValidator\\' => 29,
         ),
         'J' => 
         array (
@@ -170,10 +177,16 @@ class ComposerStaticInit01866e94038b84acd1c89df5cf260e47
             1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
             2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
         'geekcom\\ValidatorDocs\\' => 
         array (
             0 => __DIR__ . '/..' . '/geekcom/validator-docs/src/validator-docs',
         ),
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
         'Zend\\Diactoros\\' => 
         array (
             0 => __DIR__ . '/..' . '/zendframework/zend-diactoros/src',
@@ -313,6 +326,10 @@ class ComposerStaticInit01866e94038b84acd1c89df5cf260e47
         'Laravel\\Tinker\\' => 
         array (
             0 => __DIR__ . '/..' . '/laravel/tinker/src',
+        ),
+        'LaravelLegends\\PtBrValidator\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laravellegends/pt-br-validator/src/pt-br-validator',
         ),
         'JakubOnderka\\PhpConsoleHighlighter\\' => 
         array (
@@ -2024,6 +2041,8 @@ class ComposerStaticInit01866e94038b84acd1c89df5cf260e47
         'JakubOnderka\\PhpConsoleColor\\InvalidStyleException' => __DIR__ . '/..' . '/jakub-onderka/php-console-color/src/InvalidStyleException.php',
         'JakubOnderka\\PhpConsoleHighlighter\\Highlighter' => __DIR__ . '/..' . '/jakub-onderka/php-console-highlighter/src/Highlighter.php',
         'JsonSerializable' => __DIR__ . '/..' . '/nesbot/carbon/src/JsonSerializable.php',
+        'LaravelLegends\\PtBrValidator\\Validator' => __DIR__ . '/..' . '/laravellegends/pt-br-validator/src/pt-br-validator/Validator.php',
+        'LaravelLegends\\PtBrValidator\\ValidatorProvider' => __DIR__ . '/..' . '/laravellegends/pt-br-validator/src/pt-br-validator/ValidatorProvider.php',
         'Laravel\\Tinker\\ClassAliasAutoloader' => __DIR__ . '/..' . '/laravel/tinker/src/ClassAliasAutoloader.php',
         'Laravel\\Tinker\\Console\\TinkerCommand' => __DIR__ . '/..' . '/laravel/tinker/src/Console/TinkerCommand.php',
         'Laravel\\Tinker\\TinkerCaster' => __DIR__ . '/..' . '/laravel/tinker/src/TinkerCaster.php',
@@ -4150,8 +4169,14 @@ class ComposerStaticInit01866e94038b84acd1c89df5cf260e47
         'Zend\\Diactoros\\Stream' => __DIR__ . '/..' . '/zendframework/zend-diactoros/src/Stream.php',
         'Zend\\Diactoros\\UploadedFile' => __DIR__ . '/..' . '/zendframework/zend-diactoros/src/UploadedFile.php',
         'Zend\\Diactoros\\Uri' => __DIR__ . '/..' . '/zendframework/zend-diactoros/src/Uri.php',
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
         'geekcom\\ValidatorDocs\\Validator' => __DIR__ . '/..' . '/geekcom/validator-docs/src/validator-docs/Validator.php',
         'geekcom\\ValidatorDocs\\ValidatorProvider' => __DIR__ . '/..' . '/geekcom/validator-docs/src/validator-docs/ValidatorProvider.php',
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
         'phpDocumentor\\Reflection\\DocBlock' => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src/DocBlock.php',
         'phpDocumentor\\Reflection\\DocBlockFactory' => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src/DocBlockFactory.php',
         'phpDocumentor\\Reflection\\DocBlockFactoryInterface' => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src/DocBlockFactoryInterface.php',

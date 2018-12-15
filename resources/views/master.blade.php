56b0bafa50829bd6b7900c10e789d63d8bc035b2
lucineide <anderson.pereira.neves1@gmail.com>
2018-12-15 16:50:09 -0300

    select sem opçao desabilitado

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -17,8 +17,8 @@
 	<script src="/js/highcharts/modules/export-data.js"></script>
 	<script type="text/javascript" src="/js/highcharts/jquery.highchartTable.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-	
+
+
 	<style type="text/css">
 		.table-hover tbody tr:hover td, .table-hover tbody tr:hover th {
 			background-color: rgba(97, 255, 240, 0.12);
@@ -40,7 +40,7 @@
 			border-left: 1px solid #6178844f;
 			border-top: 1px solid #6178844f;
 			width: 100%;
-			
+
 		}
 		.btn-warning, .btn-warning:hover{
 			color: #fff;
@@ -58,24 +58,38 @@
 			margin-bottom: 4px;
 			margin-top: 4px;
 		}
-		
+
 		.p {
 			max-width: 10ch;
 			overflow: hidden;
 			text-overflow: ellipsis;
 			white-space: nowrap;
 		}
-		
+
 	</style>
 	<script type="text/javascript">
 		$( document ).ready(function() {
-			
+
 			$('#exampleModal').on('shown.bs.modal', function () {
 				$('[type=date').each(function(e){
 					if(this.value===''){
 						this.value=moment().format('YYYY-MM-DD');
 					}
 				});
+
+				$('select').each(function(e){
+					if($(this).find('option').length==0){
+							texto=$(this).parent().find('label').text()
+							texto=texto.replace(':','');
+							texto=texto.replace('*','');
+							msg='Adicione um '+texto;
+							console.log(msg);
+							$(this).append('<option value="">'+msg+'</option>')
+							$(this).prop( "disabled", true );
+							$(this).closest('form').find(':submit').prop( "disabled", true );
+					}
+				})
+
 			});
 			$('.data').each(function(e){
 				if(this.innerText != ''){
@@ -83,14 +97,14 @@
 					this.innerText= moment.utc(d).format("D/M/YYYY");
 				}
 			});
-			
+
 			$('#accordionExample [type=date').each(function(e){
 				if(this.value===''){
 					this.value=moment().format('YYYY-MM-DD');
 				}
 			});
-			
-			
+
+
 			$(".confirm").on("click", function(e) {
 				e.preventDefault();
 				form=this.closest('form');
@@ -110,24 +124,24 @@
 					callback:function(result){
 						if(result)
 						form.submit();
-						
+
 					}});
 				});
-				
+
 				$(".mensagem").fadeTo(2000, 500).slideUp(500, function(){
 					$(".mensagem").slideUp(500);
 				});
-				
+
 				$('.bd-user-modal-sm').css("margin-left", $(window).width() - $('.modal-content').width()*2);
-				
+
 				$('.bd-user-modal-sm').css("margin-top", $('.menu').height());
 				$('.op').css("margin",0) ;
-				
-				
+
+
 			});
-			
-			
-			
+
+
+
 		</script>
 	</head>
 	<body>
@@ -143,7 +157,7 @@
 					<img  class="btn img-fluid col-6   offset-3" src="/images/agr.png">
 					<div class="col-8 text-center offset-2" >{{$User}} </div>
 				</div>
-				
+
 				<div class="modal fade bd-user-modal-sm" tabindex="-1" role="dialog" aria-labelledby="opções" aria-hidden="true">
 					<div class="modal-dialog modal-sm op">
 						<div class="modal-content">
@@ -158,7 +172,7 @@
 									<h8>Sair</h8>
 								</a>
 							</li>
-							
+
 						</ul>
 						<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 							@csrf
@@ -169,31 +183,31 @@
 		</div>
 	</div>
 	<div  class="row">
-		
+
 		<div  class="col-2 col-sm-2 col-md-2 col-xl-2 menu">
 			<div class="row">
-				
+
 				<div class=" col-md-5 ">
 					<a href="/home">
 						<div class="row">
 							<div>
 								<img class="col-md-12 img-fluid" src="/images/Inicio.png" >
 							</div>
-							
+
 						</div>
 						<div class="row">
 							<div class="col-md-12 lead text-center">Inicio</div>
 						</div>
 					</a>
 				</div>
-				
+
 				<div class=" col-md-5 ">
 					<a href="/venda">
 						<div class="row">
 							<div>
 								<img class="col-md-12 img-fluid" src="/images/Vendas.png" >
 							</div>
-							
+
 						</div>
 						<div class="row">
 							<div  class=" col-md-12 lead text-center">Vendas</div>
@@ -208,21 +222,21 @@
 							<div>
 								<img class="col-md-12 img-fluid" src="/images/Estoque.png" >
 							</div>
-							
+
 						</div>
 						<div class="row">
 							<div  class=" col-md-12 lead     text-center">Estoque</div>
 						</div>
 					</a>
 				</div>
-				
+
 				<div class=" col-md-5 ">
 					<a href="/plantio">
 						<div class="row">
 							<div>
 								<img class="col-md-12 img-fluid" src="/images/Plantio.png" >
 							</div>
-							
+
 						</div>
 						<div class="row">
 							<div  class=" col-md-12 lead text-center">Plantio</div>
@@ -237,21 +251,21 @@
 							<div>
 								<img class="col-md-12 img-fluid" src="/images/Manejo.png" >
 							</div>
-							
+
 						</div>
 						<div class="row">
 							<div  class=" col-md-12 lead text-center">Manejo</div>
 						</div>
 					</a>
 				</div>
-				
+
 				<div class=" col-md-5 ">
 					<a href="/propriedade">
 						<div class="row">
 							<div>
 								<img class="col-md-12 img-fluid" src="/images/Propriedade.png" >
 							</div>
-							
+
 						</div>
 						<div class="row">
 							<div class="col-12 md-12 lead text-center">Propriedade</div>
@@ -266,21 +280,21 @@
 							<div>
 								<img class="col-md-12 img-fluid" src="/images/Despesas.png" >
 							</div>
-							
+
 						</div>
 						<div class="row">
 							<div  class=" col-md-12 lead text-center ">Despesas</div>
 						</div>
 					</a>
 				</div>
-				
+
 				<div class=" col-md-5 ">
 					<a href="/investimento">
 						<div class="row">
 							<div>
 								<img class="col-md-12 img-fluid" src="/images/Investimento.png" >
 							</div>
-							
+
 						</div>
 						<div class="row">
 							<div  class=" col-md-12 lead text-center">Investimentos</div>
@@ -288,7 +302,7 @@
 					</a>
 				</div>
 			</div>
-			
+
 			<div class="row">
 				<div class=" col-md-5 ">
 					<a href="/relatorio">
@@ -296,21 +310,21 @@
 							<div>
 								<img class="col-md-12 img-fluid" src="/images/Relatorio.png" >
 							</div>
-							
+
 						</div>
 						<div class="row">
 							<div  class=" col-md-12 lead text-center ">Relatorios</div>
 						</div>
 					</a>
 				</div>
-				
+
 				<div class=" col-md-5 ">
 					<a href="/manual">
 						<div class="row">
 							<div>
 								<img class="col-md-12 img-fluid" src="/images/Manual.png" >
 							</div>
-							
+
 						</div>
 						<div class="row">
 							<div  class=" col-md-12 lead text-center ">Manual</div>
@@ -318,9 +332,9 @@
 					</a>
 				</div>
 			</div>
-			
+
 		</div>
-		
+
 		<div  class="conteudo col-10 col-sm-10">
 			@isset($mensagem)
 			<div class="mensagem alert alert-{{$status}} fade in">
@@ -329,8 +343,8 @@
 			@endisset
 			@yield('conteudo')
 		</div>
-		
-		
+
+
 	</div>
 	<div class="row ">
 		<div class="rodape col-12 text-center">Desenvolvido e mantido por Leco Ufms.</div>

2a7d6bfe8e632e7f6fc2b1cea9049bf245328b79
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2018-12-18 14:18:00 -0300

    Descrição do portal.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
     <body class='new cn-sessions'>
             <div class='box-left'>
                     <div style="font:20px; height: 450px;">
-                        <p class="text-center">Lorem Ipsum isdcd,,,,,,,,,,,,,,,,,,,, simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <p class="text-center">O SIAF é um portal tem como propósito disponibilizar, para os agricultores, um local para armazenamento e gestão das informações de suas atividades desde a criação de um talhão para plantio até a sua venda para os consumidores. Além de possibilitar o controle dessas informações, o portal também busca cooperar com o agricultor dando um acompanhamento de como estão suas finanças e informações importantes para sua produção.</p>
                     </div>
                 </div>
         

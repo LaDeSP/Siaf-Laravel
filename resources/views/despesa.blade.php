d5c4e021b18d5085bd38b692655bab2b57a580fd
lucineide <anderson.pereira.neves1@gmail.com>
2018-12-19 18:03:11 -0300

    telas dispesas e investimentos

--- resources/views/despesa.blade.php
+++ resources/views/despesa.blade.php
@@ -2,10 +2,10 @@
 @section('usuario', $User)
 @section('conteudo')
     <div class="">
-    	<div class="col-10 text-right adicionar">
+    	<div class="col-12 text-right adicionar">
     		<a id="add" data-toggle="modal" data-target="#exampleModal" role="button" class="btn btn-success" style="color:white">Adicionar</a>
     	</div>
-                <div class="col-10" >
+                <div class="col-12" >
                     @if(($dados->isEmpty()))
                             <div class="text-center">
                                 <p>Por favor, adicione despesas!</p>
@@ -35,7 +35,7 @@
                                         <td>
                                             <div class="row">
                                                 <div class="col-sm-6">
-                                                    <a onclick="edit(this)" id ="editDespesa" class="btn btn-xs btn-warning" data-id="{{$d->id}}">Editar</a>                    
+                                                    <a onclick="edit(this)" id ="editDespesa" class="btn btn-xs btn-warning" data-id="{{$d->id}}">Editar</a>
                                                 </div>
                                                 <div class="col-sm-6">
                                                     <form method="post" id="investimentoDelete" action="/despesa/{{$d->id}}">
@@ -62,7 +62,7 @@
             <div class="modal-content">
 	            <div class="modal-header">
 	             <h5 class="modal-title" id="exampleModalLabel">Adicionar despesa</h5>
-            
+
                   <button type="button" class="close" data-dismiss="modal">&times;</button>
 
 	            </div>
@@ -74,7 +74,7 @@
 	                        <div class="form-group col-md-6">
 	                            <label for="propriedade_id">Propriedade</label>
 	                            <select style="-moz-appearance: none; -webkit-appearance: none; appearance: none;" form="despesa" class="form-control-plaintext" name="propriedade_id" disabled>
-	                               <option selected value="{{$propriedade->id}}" >{{$propriedade->nome}}</option>   
+	                               <option selected value="{{$propriedade->id}}" >{{$propriedade->nome}}</option>
 	                            </select>
 	                        </div>
 	                        <div class="form-group col-md-6">
@@ -135,8 +135,8 @@ function edit(elem) {
         $("form#despesa [name=propriedade_id] option").val(data.propriedade_id);
         $("form#despesa").attr('id',"despesaEdit");
         $("#exampleModal").modal('show');
-    });      
-};    
+    });
+};
 $("#despesa").submit(function( event ) {
     $('form#despesa select').removeAttr('disabled');
 });

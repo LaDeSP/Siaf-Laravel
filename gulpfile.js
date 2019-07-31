eacf51234034cbfb025268fcfdfe2897357c4429
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2019-07-31 13:34:44 -0400

    Alteração da versão 5.7 p/ 5.5 do laravel.

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var elixir = require('laravel-elixir');
-
-elixir(function (mix){
-        
-    mix.js('resources/js/app.js', 'public/js');
-    mix.sass('app.scss');
-    mix.js('resources/js/highcharts','public/js');
-});

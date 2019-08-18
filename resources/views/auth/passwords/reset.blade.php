c9fa7b5cb75b67684af1e395b608e76dc6608694
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2019-08-18 15:24:06 -0400

    Preparação da estrtura do front.

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,60 +1,43 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-   <head>
-      <link rel="icon" href="/images/favicon.png">
-      <meta charset='UTF-8'>
-      <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-      <!-- CSRF Token -->
-      <meta name="csrf-token" content="{{ csrf_token() }}">
-      <link href="/css/login.css" rel="stylesheet">
-      <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-      <title>SIAF</title>
-   </head>
-   <body class='self_registration cn-site'>
-      <div class='box-left'>
-      <div class="banner-sec img-fluid" alt="Responsive image">
+@extends('layouts.auth-master')
+
+@section('content')
+<div class="card card-primary">
+  <div class="card-header"><h4>Set a New Password</h4></div>
+
+  <div class="card-body">
+    <form method="POST" action="{{ route('password.update') }}">
+        @csrf
+        <input type="hidden" name="token" value="{{ $token }}">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" tabindex="1" value="{{ old('email') }}" autofocus>
+        <div class="invalid-feedback">
+          {{ $errors->first('email') }}
+        </div>
       </div>
-      <div class='box-right'>
-         <form class="" id="" action="{{ route('password.update') }}" accept-charset="UTF-8" method="post">
-            {{ csrf_field() }}
-            <input type="hidden" name="token" value="{{ $token }}">
-            <div class='step step1 current-step' data-step='1'>
-               <div class='title'>
-                  Redefinição de senha
-               </div>
-               <div class='form-group'>
-                  <label>
-                  Email
-                  <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
-                  @if ($errors->has('email'))
-                  <span class="invalid-feedback" role="alert">
-                  <strong>{{ $errors->first('email') }}</strong>
-                  </span>
-                  @endif
-                  </label>
-               </div>
-               <div class='form-group'>
-                  <label>
-                  Senha
-                  <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-                  @if ($errors->has('password'))
-                  <span class="invalid-feedback" role="alert">
-                  <strong>{{ $errors->first('password') }}</strong>
-                  </span>
-                  @endif
-                  </label>
-               </div>
-               <div class='form-group'>
-                  <label>
-                  Confirme a senha
-                  <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                  </label>
-               </div>
-               <button class='active btn btn-success' id='submit-form' type='submit'>
-               Redefinir Senha
-               </button>
-            </div>
-         </form>
+      <div class="form-group">
+        <label for="password" class="control-label">Password</label>
+        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid': '' }}" name="password" tabindex="2">
+        <div class="invalid-feedback">
+          {{ $errors->first('password') }}
+        </div>
       </div>
-   </body>
-</html>
+      <div class="form-group">
+        <label for="password_confirmation" class="control-label">Confirm Password</label>
+        <input id="password_confirmation" type="password" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid': '' }}" name="password_confirmation" tabindex="2">
+        <div class="invalid-feedback">
+          {{ $errors->first('password_confirmation') }}
+        </div>
+      </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+          Set a New Password
+        </button>
+      </div>
+    </form>
+  </div>
+</div>
+<div class="mt-5 text-muted text-center">
+  Recalled your login info? <a href="{{ route('login') }}">Sign In</a>
+</div>
+@endsection

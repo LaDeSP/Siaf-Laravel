ffed65cfc65cce5e697682498f9198fed42496ac
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2019-09-05 18:02:47 -0400

    config validation.

--- app/Http/Requests/TalhaoFormRequest.php
+++ app/Http/Requests/TalhaoFormRequest.php
@@ -14,7 +14,7 @@ class TalhaoFormRequest extends FormRequest{
     public function rules(){
         return [
             'nome_talhao'      =>  'required|max:255',
-            'area_talhao'     =>  'required|integer',
+            'area_talhao'     =>  'required|integer|min:1',
         ];
     }
 
@@ -23,6 +23,7 @@ class TalhaoFormRequest extends FormRequest{
             'nome_talhao.required'     => 'Nome do talhão é um campo obrigatório!',
             'area_talhao.required' => 'Área do talhão é um campo obrigatório!',
             'area_talhao.integer'     =>  'Área do talhão deve ser um número inteiro!',
+            'area_talhao.min'     =>  'Área do talhão deve ser maior que zero!',
         ];
     }
 }

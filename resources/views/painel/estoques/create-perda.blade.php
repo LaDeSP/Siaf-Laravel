ce6deeb7b79d6cc334a70ce5a097d0817322ab77
Denner09 <denner.sr@gmail.com>
2019-09-24 16:50:18 -0400

    Criação dos formulários de perda do plantio e estoque

--- resources/views/painel/estoques/create-perda.blade.php
+++ resources/views/painel/estoques/create-perda.blade.php
@@ -37,53 +37,53 @@ Adicionar Manejo
                         {{ csrf_field() }}
                         <div class="card-body">
                             <div class="form-group">
-                                <label>Manejo<span class="text-danger">*</span></label>
+                                    <label>Data<span class="text-danger">*</span></label>
+                                    <input name="data_manejo" type="date" class="form-control {{ $errors->has('data_manejo') ? ' is-invalid' : '' }}" required="">
+                                    <div class="invalid-feedback">
+                                        Qual foi a data da perda?
+                                    </div>
+                                    @if ($errors->has('data_manejo'))
+                                    <div class="invalid-feedback">
+                                        {{ $errors->first('data_manejo') }}
+                                    </div>
+                                    @endif
+                            </div>  
+                            <div class="form-group">
+                                <label>Destino<span class="text-danger">*</span></label>
                                 <select name="manejo" class="custom-select form-control {{ $errors->has('manejo') ? ' is-invalid' : '' }}" required value="{{ old('manejo') }}">
-                                    <option selected="" value="">Selecione um manejo</option>
+                                    <option selected="" value="">Selecione um destino</option>
                                     @foreach ($destinos as $destino)
                                     <option value="{{$destino->id}}">{{$destino->nome}}</option>
                                     @endforeach
                                 </select>
                                 <div class="invalid-feedback">
-                                    Manejo é obrigatório!
+                                    Destino é obrigatório!
                                 </div>
                                 @if ($errors->has('manejo'))
                                 <div class="invalid-feedback">
                                     {{ $errors->first('manejo') }}
                                 </div>
                                 @endif
-                            </div>  
+                            </div> 
                             <div class="form-group">
-                                <label>Descrição</span></label>
-                                <input name="descricao" type="text" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label>Horas Utilizadas<span class="text-danger">*</span></label>
+                                <label>Quantidade<span class="text-danger">*</span></label>
                                 <input name="horas_utilizadas" type="number" min="1" class="form-control {{ $errors->has('horas_utilizadas') ? ' is-invalid' : '' }}" required="" placeholder="Ex: 5">
                                 <div class="invalid-feedback">
-                                    Quantas horas foram utilizadas no manejo?
+                                    Quantos produtos foram perdidos?
                                 </div>
                                 @if ($errors->has('horas_utilizadas'))
                                 <div class="invalid-feedback">
                                     {{ $errors->first('horas_utilizadas') }}
                                 </div>
                                 @endif
-                            </div>
+                            </div>    
                             <div class="form-group">
-                                <label>Data<span class="text-danger">*</span></label>
-                                <input name="data_manejo" type="date" class="form-control {{ $errors->has('data_manejo') ? ' is-invalid' : '' }}" required="">
-                                <div class="invalid-feedback">
-                                    Qual foi a data do manejo?
-                                </div>
-                                @if ($errors->has('data_manejo'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('data_manejo') }}
-                                </div>
-                                @endif
-                            </div>          
+                                <label>Descrição</span></label>
+                                <input name="descricao" type="text" class="form-control">
+                            </div>     
                         </div>
                         <div class="card-footer text-center">
-                            <button class="btn btn-success">Cadastrar Manejo</button>
+                            <button class="btn btn-success">Cadastrar Perda</button>
                         </div>
                     </form>
                 </div>
@@ -92,4 +92,3 @@ Adicionar Manejo
     </div>
 </section>
 @endsection
-
